2eaef0b57b85d5a1ffbe16301be086e14031f210
j1mmy7z7 <jamesmuchiri792@yahoo.com>
2024-12-14 17:48:59 +0300

    update: render all html files in the template folder

--- web/home.go
+++ web/home.go
@@ -3,20 +3,26 @@ package web
 import (
 	"net/http"
 	"html/template"
+	"log"
 )
 
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
+var templates *template.Template
+var err error
 
-	tmpl, err := template.ParseFiles("./assets/template/index.html")
-			if err != nil {
-				http.Error(w, "Error: cannot load login page", http.StatusInternalServerError)
-				return
+func init() {
+	templates, err = template.ParseGlob("./assets/template/*.html")
+	if err != nil {
+		log.Fatalf("Failed to render template")
+		return
 	}
+}
 
-	err = tmpl.Execute(w, nil)
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		err = templates.ExecuteTemplate(w, "index.html", nil)
 		if err != nil {
-			http.Error(w, "Error: cannot load login page", http.StatusInternalServerError)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-	
+	}
 } 
