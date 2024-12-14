8357b85f59a7dfe1c5fd1631e558a58dca243d33
rabinnnn <otiinibar@gmail.com>
2024-12-14 16:56:09 +0300

    refactor: remove unused lines

--- main.go
+++ main.go
@@ -1,35 +1,12 @@
 package main
 
 import (
-	"database/sql"
-	"encoding/json"
 	"Sano/database"
-	"fmt"
-	"log"
-	"math/rand"
-	"net/http"
-	"time"
-
 	_ "github.com/mattn/go-sqlite3"
 
 )
 
-var db *sql.DB
-
-func init() {
-	_, err = db.Exec(createTable)
-	if err != nil {
-		log.Fatalf("Error creating table: %v\n", err)
-	}
-}
-
 func main(){
 	db.InitDB()
 	db.CreateTable()
-	http.HandleFunc("/login", handler.LoginHandler)
-
-	fmt.Println("Server is running on http://localhost:9000")
-	if err := http.ListenAndServe(":9000", nil); err != nil {
-		log.Fatalf("Server failed to start: %v", err)
-	}
 }
