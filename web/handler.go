fa0260cbbd59b75abf2c70c8b0230eb0adaa5e8a
wnjuguna <wm453856@gmail.com>
2024-12-14 15:07:42 +0300

    feat: Code refactoring and cleaning

--- web/handler.go
+++ web/handler.go
@@ -2,15 +2,43 @@ package web
 
 import (
 	"context"
+	"crypto/rand"
+	"encoding/json"
 	"fmt"
 	"io"
+	"log"
+	"math/big"
 	"net/http"
 	"path/filepath"
+	"time"
 
 	"github.com/google/uuid"
 	"google.golang.org/api/drive/v3"
 	"google.golang.org/api/option"
 )
+
+type LoginRequest struct {
+	PhoneNo string `json:"phone_no"`
+	IDNo    string `json:"ID_no"`
+}
+
+type OTPResponse struct {
+	Message string `json:"message"`
+}
+
+func generateOTP() (string, error) {
+	max := big.NewInt(1000000)
+	otp, err := rand.Int(rand.Reader, max)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%06d", otp), nil
+}
+
+func sendOTP(phoneNumber, otp string) error {
+	log.Printf("Sending OTP code %s to phone number %s\n", otp, phoneNumber)
+	return nil
+}
 
 func UploadHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
@@ -65,4 +93,52 @@ func uploadFileToGoogleDrive(fileName string, file io.Reader) (string, error) {
 	}
 
 	return uploadedFile.Id, nil
+}
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var req LoginRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	var exsist bool
+	err := db.QueryRow(`SELECT EXSISTS(SELECT 1 FROM users WHERE phone_number=?)`,
+		req.PhoneNo, req.IDNo).Scan(&exsist)
+	if err != nil {
+		http.Error(w, "Error checking user", http.StatusInternalServerError)
+		return
+	}
+
+	if !exsist {
+		http.Error(w, "Invalid Phone number or ID number", http.StatusUnauthorized)
+		return
+	}
+
+	otp, _ := generateOTP()
+	expiry := time.Now().Add(5 * time.Minute)
+
+	_, err = db.Exec(`UPDATE users SET otp_code=?, otp_expiry=? WHERE phone_number=?`, otp, expiry, req.PhoneNo)
+	if err != nil {
+		http.Error(w, "Error uptdating OTP", http.StatusInternalServerError)
+		return
+	}
+
+	if err := sendOTP(req.PhoneNo, otp); err != nil {
+		http.Error(w, "Error sending OTP", http.StatusInternalServerError)
+		return
+	}
+
+	response := OTPResponse{
+		Message: "OTP sent successfully",
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+
+	http.Redirect(w, r, "/Dashboard", http.StatusSeeOther)
 }
