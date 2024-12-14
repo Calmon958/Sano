1f823b90a7461fb118f3939387b161b16832adb8
rabinnnn <otiinibar@gmail.com>
2024-12-14 23:44:57 +0300

    feat: call handlers for doctor registration

--- main.go
+++ main.go
@@ -13,8 +13,12 @@ func main() {
 	db.InitDB()
 	db.CreateTable()
 
-	http.HandleFunc("/login", handler.LoginHandler)
+	//http.HandleFunc("/doctorRegistration", handler.RegisterGp)
+
+	http.HandleFunc("/register", handler.RegisterGp)
+	//http.HandleFunc("/login", handler.LoginHandler)
 	http.HandleFunc("/", handler.HomeHandler)
+	http.HandleFunc("/doctor_registration", handler.RegisterGp)
 	fmt.Println("Server is running on http://localhost:9000")
 	if err := http.ListenAndServe(":9000", nil); err != nil {
 		log.Fatalf("Server failed to start: %v", err)
